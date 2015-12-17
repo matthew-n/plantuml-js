994f015a07dbe5626f329e5f70d80f76a96a7f36
Matt Naul <matthew.naul@gmail.com>
2015-12-17 17:29:06 -0600

    add test for annotations

---
+++ test/annotation.js
@@ -0,0 +1,54 @@
+var expect =  require('chai').expect;
+var fs = require('fs');
+var PEG = require('pegjs');
+
+describe('PlantUML Diagram Annotations', function() {
+
+	var	parser;
+	
+	before(function(){
+		var grammar;
+		grammar = fs.readFileSync('./src/PlantUML.pegjs', 'utf8');
+		parser = PEG.buildParser(grammar);
+	});
+	
+	describe('Singleton Annotaitons', function(){
+		it('header', function(){
+			var parsed = parser.parse('@startuml\n Header My Project Docs endheader\n@enduml');
+		});
+		
+		it('footer', function(){
+			var parsed = parser.parse('@startuml\n footer Copyright 2014 Example, Inc. endfooter \n@enduml');
+		});
+		
+		it('legend', function(){
+			var text =  '@startuml\n '
+				+'legend right\n'
+				+'Primary Key - PK \n'
+				+'Foreign Key - FK \n'
+				+'Natural Key - NK \n'
+				+'end legend \n'
+				+'@enduml';
+			var parsed = parser.parse(text);
+		});
+		
+		it('title', function(){
+			var parsed = parser.parse('@startuml\n title Biz.buz Diagram <<Physical Data Model>> \n@enduml');
+		});
+	});
+	
+	describe('Notes', function(){
+		it('basic note', function(){
+			var parsed = parser.parse('@startuml\n note: this is a simple note \n@enduml');
+		});
+	});
+
+/*
+note as n1
+Assume column are {NOT NULL} and 
+that indexes exist for all <<PK>> 
+and <<FK>> columns
+end note
+*/
+	
+});// end annotation diagram
